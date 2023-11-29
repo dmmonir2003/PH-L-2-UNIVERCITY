d90c0e64a7a7a689c10dc0e53a35785a4ee33f9b
dmmonir2003 <dmmonir2003@gmail.com>
2023-11-30 04:14:52 +0600

    test

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -2,6 +2,20 @@ import { TAcademicSemester } from './academicSemester.interface';
 import { AcademicSemesterModel } from './academicSemester.model';
 
 const createAcademicSemesterInDB = async (payLoad: TAcademicSemester) => {
+  type TAcademicSemesterNameCodeMapper = {
+    [key: string]: string;
+  };
+
+  const AcademicSemesterNameCodeMapper: TAcademicSemesterNameCodeMapper = {
+    autumn: '01',
+    summer: '02',
+    fall: '03',
+  };
+
+  if (AcademicSemesterNameCodeMapper[payLoad.name] !== payLoad.code) {
+    throw new Error('semester code is invalid');
+  }
+
   const result = await AcademicSemesterModel.create(payLoad);
   return result;
 };
